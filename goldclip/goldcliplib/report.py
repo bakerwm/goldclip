3d9b204313c16529b177415be67cafe1a53daacd
bakerwm <wangm08@hotmail.com>
2018-07-17 22:35:48 +0800

    add reporter

---
+++ goldclip/goldcliplib/report.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+make a summary of the project
+1. statistic reads trimming
+2. reads mapping
+3. peaks
+4. rtstops
+5. figures
+
+functions
+"""
+
+__author__ = "Ming Wang"
+__email__ = "wangm08@hotmail.com"
+__date__ = "2018-03-21"
+__version__ = "0.1"
